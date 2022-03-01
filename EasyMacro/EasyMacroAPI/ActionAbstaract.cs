39aa2f782fe95602ff1c451139046974cd83bc93
juheun <juheun1997@naver.com>
2022-03-01 14:27:25 +0900

    시작단계

---
+++ EasyMacro/EasyMacroAPI/ActionAbstaract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMacroAPI
+{
+    public class ActionAbstaract
+    {
+        int x, y;
+        Action action;
+        float time;
+    }
+}
--- EasyMacro/EasyMacroAPI/MacroManager.cs
+++ EasyMacro/EasyMacroAPI/MacroManager.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace EasyMacroAPI
 {
     public class MacroManager
     {
         private static MacroManager instance = null;
+
+        private ArrayList arrayList;
 
         public static MacroManager Instance()
         {

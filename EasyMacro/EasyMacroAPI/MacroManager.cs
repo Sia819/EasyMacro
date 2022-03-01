edd860ce88a7657c03b8fd447106affe76874daa
juheun <juheun1997@naver.com>
2022-03-01 15:56:04 +0900

    이주흔 03-01

--- EasyMacro/EasyMacroAPI/MacroManager.cs
+++ EasyMacro/EasyMacroAPI/MacroManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading;
 
 namespace EasyMacroAPI
@@ -27,6 +29,17 @@ namespace EasyMacroAPI
         private static void Init()
         {
             macroThread.IsBackground = true;
+        }
+
+        public void InsertList(ActionAbstract insertAction)
+        {
+            insertAction.index = actionList.Count;
+            actionList.Add(insertAction);
+        }
+        
+        public void DeleteList(int index)
+        {
+            actionList.RemoveAt(index);
         }
 
         public void StartMacro()
@@ -57,6 +70,32 @@ namespace EasyMacroAPI
                         return;
                 }
             }
+        }
+
+        public void SaveData()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("C:\\emsave.em", FileMode.Create);
+
+            SerializableDataField filesaver = new SerializableDataField();
+
+            filesaver.actionList = actionList;
+
+            bf.Serialize(fs, filesaver);
+            fs.Close();
+        }
+
+        public void LoadData()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("C:\\emsave.em", FileMode.Open);
+
+            SerializableDataField filesaver = new SerializableDataField();
+
+            filesaver = bf.Deserialize(fs) as SerializableDataField;
+            fs.Close();
+
+            actionList = filesaver.actionList;
         }
     }
 }

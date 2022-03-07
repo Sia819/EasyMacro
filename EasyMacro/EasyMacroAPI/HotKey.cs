f4b8062dcc1faed0da72e7b4e61532ff4076adfe
juheun <71806600+juheun@users.noreply.github.com>
2022-03-07 13:31:03 +0900

    hotkey 추가

---
+++ EasyMacro/EasyMacroAPI/HotKey.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EasyMacroAPI
+{
+    class HotKey
+    {
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        int handle;
+
+        public HotKey()
+        {
+            handle = FindWindow(null, "MainWindow");
+        }
+
+        const int HOTKEY_ID = 31197; //Any number to use to identify the hotkey instance
+
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Windows = 8
+        }
+
+        const int WM_HOTKEY = 0x0312;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 핫키 등록
+            //RegisterHotKey((IntPtr)handle, HOTKEY_ID, KeyModifiers.Control | KeyModifiers.Shift, Keys.N);
+            RegisterHotKey((IntPtr)handle, HOTKEY_ID, KeyModifiers.None, Keys.F9);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //핫키 해제
+            UnregisterHotKey((IntPtr)handle, HOTKEY_ID);
+        }
+    }
+}

e19d19a80cf541bad12b70d3a3dbfb3529b5c95f
juheun <71806600+juheun@users.noreply.github.com>
2022-03-14 12:40:45 +0900

    CaptureManager 추가

---
+++ EasyMacro/EasyMacroAPI/CaptureManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace EasyMacroAPI
+{
+    class CaptureManager
+    {
+        public void ScreenCapture()
+        {
+            Bitmap bmp = new Bitmap(500, 500,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics gr = Graphics.FromImage(bmp);
+
+            gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+        }
+    }
+}

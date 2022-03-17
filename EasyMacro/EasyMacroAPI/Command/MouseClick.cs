0fec8cd577ea951502b5c688609575927cc6b56d
MinSeokKim33 <100578402+MinSeokKim33@users.noreply.github.com>
2022-03-17 22:41:13 +0900

    MouseClick클래스 생성
    do는 아직 구현 못함....

---
+++ EasyMacro/EasyMacroAPI/Command/MouseClick.cs
@@ -0,0 +1,29 @@
+﻿using EasyMacroAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMacroAPI.Command
+{
+    public class MouseClick : IAction
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public MacroTypes MacroType => MacroTypes.MouseClick;
+
+        public MouseClick(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Do()
+        {
+            
+        }
+
+
+    }
+}

d6a4caeec53eccc2c05ca31d8a32b60ab965a6a4
juheun <juheun1997@naver.com>
2022-05-22 21:05:42 +0900

    Update ImageManagerViewModel.cs

--- EasyMacro/EasyMacro/ViewModel/ImageManagerViewModel.cs
+++ EasyMacro/EasyMacro/ViewModel/ImageManagerViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -18,8 +19,7 @@ namespace EasyMacro.ViewModel
 
     public partial class ImageManagerViewModel
     {
-
-
+        Dictionary<string, Bitmap> imgDict = new Dictionary<string, Bitmap>();
 
         private void ImageAddCommand()
         {
@@ -29,11 +29,16 @@ namespace EasyMacro.ViewModel
             {
                 if (Directory.Exists(ImageFilePath)) // TextBox에 있는것이 존재하는 폴더
                 {
-
+                    dlg.InitialDirectory = ImageFilePath;
                 }
                 else if (File.Exists(ImageFilePath)) // TextBox에 있는것이 파일
                 {
                     // 이미지 파일인지 체크
+                    if(Path.GetExtension(ImageFilePath) == ".jpg" || Path.GetExtension(ImageFilePath) == ".png")
+                    {
+                        imgDict.Add(ImageFilePath, new Bitmap(ImageFilePath));
+                        return;
+                    }
                 }
             }
 
@@ -61,14 +66,16 @@ namespace EasyMacro.ViewModel
             if (result == true)
             {
                 // Open document 
-                string fileName = dlg.FileName;
-                // Do something with fileName  
+                ImageFilePath = dlg.FileName;
+                // Do something with fileName
+                imgDict.Add(ImageFilePath, new Bitmap(ImageFilePath));
             }
         }
 
-
-
-
+        private void ImageDelCommand(string path)
+        {
+            imgDict.Remove(path);
+        }
 
     }
 }
